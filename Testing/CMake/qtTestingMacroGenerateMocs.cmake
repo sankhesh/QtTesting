766c8d42451185dd9ae7d847814cab5a2bd6e776
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2014-01-14 14:37:46 -0500

    Changed all qt4 macros to qt5 macros
    Replaced all instances of finding Qt4 package to Qt5 package.
    Replaced all Qt4 cmake macros to Qt5 cmake macros.

--- Testing/CMake/qtTestingMacroGenerateMocs.cmake
+++ Testing/CMake/qtTestingMacroGenerateMocs.cmake
@@ -1,10 +1,10 @@
 
-# QT4_GENERATE_MOCS(inputfile1 [inputfile2 ...])
+# QT5_GENERATE_MOCS(inputfile1 [inputfile2 ...])
 
-macro(QT4_GENERATE_MOCS)
+macro(QT5_GENERATE_MOCS)
   foreach(file ${ARGN})
     set(moc_file moc_${file})
-    QT4_GENERATE_MOC(${file} ${moc_file})
+    QT5_GENERATE_MOC(${file} ${moc_file})
     macro_add_file_dependencies(${file} ${CMAKE_CURRENT_BINARY_DIR}/${moc_file})
   endforeach()
 endmacro()
