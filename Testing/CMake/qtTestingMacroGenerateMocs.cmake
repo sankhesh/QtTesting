ce2d8584d8b7dccd5c1a78863f3a3aa338a9c159
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2014-01-14 15:07:28 -0500

    macro_add_file_depends replaced by object_depends as recommended.

--- Testing/CMake/qtTestingMacroGenerateMocs.cmake
+++ Testing/CMake/qtTestingMacroGenerateMocs.cmake
@@ -4,8 +4,9 @@
 macro(QT5_GENERATE_MOCS)
   foreach(file ${ARGN})
     set(moc_file moc_${file})
-    QT5_GENERATE_MOC(${file} ${moc_file})
-    macro_add_file_dependencies(${file} ${CMAKE_CURRENT_BINARY_DIR}/${moc_file})
+    qt5_generate_moc(${file} ${moc_file})
+    set_property(SOURCE ${file} APPEND PROPERTY
+      OBJECT_DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${moc_file})
   endforeach()
 endmacro()
 
